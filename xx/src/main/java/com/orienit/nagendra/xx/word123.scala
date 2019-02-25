eb8936be8a7fdccfe3debab0709862bd213e61fd
Nagendra3824 <nagendra3824@gmail.com>
2019-02-25 17:24:46 +0530

    word

---
+++ xx/src/main/java/com/orienit/nagendra/xx/word123.scala
@@ -0,0 +1,17 @@
+package com.orienit.nagendra.xx
+
+import org.apache.spark.sql.SparkSession
+
+object word123 {
+  def main(args: Array[String]): Unit = {
+    
+    
+    val spark = SparkSession.builder().master("local").appName("prasad spark").getOrCreate()
+    
+    
+    val text = spark.sparkContext.textFile("/home/kalyan/work/hi")
+    val counts = text.flatMap(line => line.split(" ")).map(word => (word,1)).reduceByKey(_+_)
+    counts.saveAsTextFile("/home/kalyan/work/op4")
+    
+  }
+}
